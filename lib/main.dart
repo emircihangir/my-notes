35e11ed4c74f6513474d0c024ba994615eaddcda
Muhammed Emir Cihangir <69801024+emircihangir@users.noreply.github.com>
2025-06-14 18:50:27 +0300

    Add macOS window configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:macos_ui/macos_ui.dart';
 import 'package:mynotes/mac-app.dart';
 import 'package:mynotes/android-app.dart';
 
-void
-main() {
+/// This method initializes macos_window_utils and styles the window.
+Future<void> _configureMacosWindowUtils() async {
+  const config = MacosWindowUtilsConfig();
+  await config.apply();
+}
+
+Future<void> main() async {
   if (Platform.isMacOS) {
-    runApp(
-      macApp(),
-    );
+    await _configureMacosWindowUtils();
+    runApp(macApp());
   } else if (Platform.isAndroid) {
-    runApp(
-      androidApp(),
-    );
+    runApp(androidApp());
   }
 }
