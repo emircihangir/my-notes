6766c013409804a49b7533945c07a63834391d2d
Muhammed Emir Cihangir <69801024+emircihangir@users.noreply.github.com>
2025-06-14 19:08:49 +0300

    Create the scaffold of the app

--- lib/mac-app.dart
+++ lib/mac-app.dart
@@ -1,6 +1,46 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:macos_ui/macos_ui.dart';
 
 Widget
 macApp() {
-  return Placeholder();
+  return MacosApp(
+    title: "My Notes",
+    theme: MacosThemeData.light(),
+    darkTheme: MacosThemeData.dark(),
+    themeMode: ThemeMode.system,
+    home: MacosWindow(
+      child: Builder(
+        builder:
+            (
+              context,
+            ) {
+              return MacosScaffold(
+                toolBar: const ToolBar(
+                  title: Text(
+                    'My Notes',
+                    textAlign: TextAlign.center,
+                  ),
+                  automaticallyImplyLeading: false,
+                  centerTitle: true,
+                ),
+                children: [
+                  ContentArea(
+                    builder:
+                        (
+                          context,
+                          scrollController,
+                        ) {
+                          return Center(
+                            child: Text(
+                              "Hi",
+                            ),
+                          );
+                        },
+                  ),
+                ],
+              );
+            },
+      ),
+    ),
+  );
 }
