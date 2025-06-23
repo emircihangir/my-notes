cc42af8f04e1d1bf5450b18b9d2b61d06ad040df
Muhammed Emir Cihangir <69801024+emircihangir@users.noreply.github.com>
2025-06-23 12:30:47 +0300

    Remove macos_ui related code from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,14 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:macos_ui/macos_ui.dart';
 import 'package:mynotes/mac-app/changenotifiers/minor-variables-model.dart';
 import 'package:mynotes/mac-app/changenotifiers/notes-model.dart';
 import 'package:mynotes/mac-app/mac-app.dart';
 import 'package:mynotes/android-app.dart';
 import 'package:provider/provider.dart';
 
-/// This method initializes macos_window_utils and styles the window.
-Future<
-  void
->
-_configureMacosWindowUtils() async {
-  const config = MacosWindowUtilsConfig();
-  await config.apply();
-}
-
-Future<
-  void
->
+void
 main() async {
   if (Platform.isMacOS) {
-    await _configureMacosWindowUtils();
     runApp(
       MultiProvider(
         providers: [
