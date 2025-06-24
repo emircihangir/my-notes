09ba1c0f74ab96faa15aed083461e17dc106e0c0
Muhammed Emir Cihangir <69801024+emircihangir@users.noreply.github.com>
2025-06-24 19:16:25 +0300

    Add strikethrough text widget

--- lib/mac-app/text-renderer-widget.dart
+++ lib/mac-app/text-renderer-widget.dart
@@ -5,13 +5,17 @@ Widget boldTextWidget({required String data}) =>
 Widget bulletTextWidget({required String data}) => Text(data.replaceFirst("- ", "\u2022 "));
 Widget tagTextWidget({required String data}) =>
     Text(data.replaceFirst("# ", ""), style: TextStyle(color: Colors.black.withAlpha(100)));
+Widget strikeThroughTextWidget({required String data}) => Text(
+  data.replaceFirst("-* ", ""),
+  style: TextStyle(decoration: TextDecoration.lineThrough, color: Colors.black.withAlpha(100)),
+);
 
 Widget textRenderer({required String data}) {
   List<Widget> textBlocks = [];
   List<String> dataLines = data.split("\n");
   for (var dataLine in dataLines) {
     if (dataLine.trimLeft().startsWith("* ")) {
-      // bold text
+      // bold
       textBlocks.add(boldTextWidget(data: dataLine));
     } else if (dataLine.trimLeft().startsWith("- ")) {
       // bullet list
@@ -19,6 +23,9 @@ Widget textRenderer({required String data}) {
     } else if (dataLine.trimLeft().startsWith("# ")) {
       // tag
       textBlocks.add(tagTextWidget(data: dataLine));
+    } else if (dataLine.trimLeft().startsWith("-* ")) {
+      // strikethrough
+      textBlocks.add(strikeThroughTextWidget(data: dataLine));
     } else {
       // plain text
       textBlocks.add(Text(dataLine));
